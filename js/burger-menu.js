63f9ceb013a0f141878425a2a989eb2bea908701
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2023-12-28 21:48:53 +0100

    add animation

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,7 @@
 const header = document.querySelector("header");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const menu = document.querySelector(".header__menu");
 
 const toggleMenu = () => header.classList.toggle("mobile-menu");
 const disableScroll = () =>
@@ -10,6 +11,12 @@ menuBtnOpen.addEventListener("click", () => {
   toggleMenu();
   disableScroll();
 });
+
+menu.addEventListener("click", () => {
+  document.body.classList.remove("is-scroll-disabled");
+  header.classList.remove("mobile-menu");
+});
+
 menuBtnClose.addEventListener("click", () => {
   toggleMenu();
   disableScroll();
